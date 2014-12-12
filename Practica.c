177ac1efee4510e981c64f1595cc188d12efefdc
Alberto <Alberto@MBPA.local>
2014-12-12 11:07:25 +0100

    escrito el código de esPrimo y del hilo productor

--- Practica.c
+++ Practica.c
@@ -1,17 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
 #include <pthread.h>
 #include <semaphore.h>
 
 int *buffer;
-int n_numeros;
+int nnumeros;
+
+bool esPrimo (int num) {
+	int divs = 2;
+
+	while (divs <= sqrt (num)) {
+		if (num % divs == 0) { return false; }
+		else { divs++; }
+	}
+
+	return true;
+}
 
 void *productor (void *arg) {
-	int num_gen = rand() % 100000;
+	int *args = (int *) arg, i;
+
+	for (i = 0; i < args[1]; i++) {
+		buffer[i % args[2]] = rand() % 100000;
+	}
+
+	pthread_exit(NULL);
 }
 
 void *consumidor (void *arg) {
 	int id = *((int *) arg);
+
+
 }
 
 int main (int argc, char *argv[]) {
@@ -32,30 +53,30 @@ int main (int argc, char *argv[]) {
 			exit(2);
 		}
 	}
+	if (args[2] > args[1]/2) {
+		fprintf (stderr, "ERROR: El argumento TamBuffer es demasiado grande");
+		exit(3);
+	}
 
 	// Asignamos el tamaño al buffer reservando memoria dinamica
 	buffer = (int *) malloc (sizeof (int) * args[2]);
 	if (buffer == NULL) {
 		fprintf (stderr, "ERROR: Fallo al reservar memoria\n");
-		exit(3);
+		exit(4);
 	}
-
-	n_numeros = args[1];
 
 	int num_hilo[args[0]];
 	pthread_t productor, consumidor[args[0]];
 
 	/* Creamos el hilo productor y los Nhilos consumidores 
 		usando num_hilo para pasar argumentos de forma segura */
-	pthread_create (&productor, NULL, productor, (void *) NULL);
+	pthread_create (&productor, NULL, productor, (void *) args);
 	for (i = 0; i < args[0];i++) {
-		num_hilo[i] = i;
+		num_hilo[i] = i+1;
 	}
 	for (i = 0; i < args[0]; i++) {
 		pthread_create (&consumidor[i], NULL, consumidor, (void *) &num_hilo[i]);
 	}
 
-
-
-	return 1;
+	return 0;
 }
